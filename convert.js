cfcd946c79cce221023a938d47ea826a27c3bb96
Thaddee Tyl <thaddee.tyl@gmail.com>
2014-05-17 19:44:30 +0200

    Automatic dependency inclusion.

--- convert.js
+++ convert.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var path = require('path');
 var dot = require('dot');
 
 function fetchHighlightRules(aceSheet) {
@@ -26,18 +27,84 @@ function fetchHighlightRules(aceSheet) {
   return result;
 }
 
+// Given a file located at `filePath` containing `text` (which includes a
+// define() clause), take out the define() clause into a format we can use.
+function fetchDefine(filePath, text) {
+  // Ignore all lines until define(), and take everything from there
+  // up to the end of the file.
+  var lines = (''+text).split('\n');
+  var beforeDefine = true;
+  var result = '  define(' + JSON.stringify(filePath)
+      + ', function(require, exports, module) {\n';
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    if (beforeDefine) {
+      if (line.match(/^define\(/)) {
+        // We have already added the define() start we wanted. Skipping it.
+        beforeDefine = false;
+      }
+    } else { result += '    ' + line + '\n'; }
+  }
+  return result;
+}
+
+// Functions to find all dependencies.
+
+// Given a define() module, return a list of all paths that are require()'d.
+function fetchRequire(text) {
+  var requires = [];
+  var lines = (''+text).split('\n');
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    var match = line.match(/var .+ = require\((.+)\)/);
+    if (match != null) {
+      requires.push(JSON.parse(match[1]) + '.js');
+    }
+  }
+  return requires;
+}
+
+// Return a list of the code for each `define()` module
+// and the modules it requires, recursively.
+// modulePath: string path of the module to fetch.
+// text: source code of the module.
+function fetchModule(modulePath, text, modules, hasModules) {
+  modules = modules || [];
+  hasModules = hasModules || Object.create(null);
+  // Fetch the requirements.
+  var requires = fetchRequire(text);
+  for (var i = 0; i < requires.length; i++) {
+    var file = fs.readFileSync(path.join(aceHighlightDir, requires[i]));
+    fetchModule(requires[i], file, modules, hasModules);
+  }
+  // Store the current module.
+  if (hasModules[modulePath] === undefined) {
+    // This module hasn't been included yet.
+    modules.push(fetchDefine(modulePath, text));
+    hasModules[modulePath] = true;
+  }
+  return modules;
+}
+
+
+// Read arguments.
+
+
 var modeConf = {};
 
 // Read all arguments.
+// TODO: remove the line below.
 // eg, ./ace2cm asciidoc ./asciidoc_highlight_rules.js '{"textRules":false}'
+// eg, ./ace2cm asciidoc /path/to/ace/ '{"modeName":"asciidoc"}'
 
 // The mode name is first.
 modeConf.modeName = process.argv[2] || 'mode';
-modeConf.rules = fetchHighlightRules(fs.readFileSync(process.argv[3]));
+modeConf.aceDir = process.argv[3];
 
 // Defaults.
+modeConf.aceName = modeConf.modeName;
 modeConf.textRules = true;
-modeConf.mixedMode = false;
+modeConf.mixedMode = false;  // FIXME: implement mixed mode.
 
 // The last argument is a JSON object.
 function addOtherConf(modeConf, otherConf) {
@@ -46,6 +113,17 @@ function addOtherConf(modeConf, otherConf) {
   }
 }
 addOtherConf(modeConf, JSON.parse(process.argv[4] || '{}'));
+
+var aceHighlightRelativeDir = 'lib/ace/mode';
+var aceHighlightDir = path.join(modeConf.aceDir, aceHighlightRelativeDir);
+var aceHighlightBasename = modeConf.aceName + '_highlight_rules';
+var aceHighlightFile = fs.readFileSync(path.join(aceHighlightDir,
+    aceHighlightBasename + '.js'));
+// Find all required modules' path.
+modeConf.rules = fetchHighlightRules(aceHighlightFile);
+modeConf.dependencies = fetchModule(aceHighlightBasename,
+    aceHighlightFile).join('\n');
+
 
 // Fetch the template.
 var template = ''+fs.readFileSync('mode-template.js');
